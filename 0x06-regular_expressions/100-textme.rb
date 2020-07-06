4f76381b10b427c1f49d236c84d7b0e81a881b69
yacinekedidi <kedidiyacine@gmail.com>
2020-07-06 20:58:25 +0100

    rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
+# a = ARGV[0].scan(/(?<=from:)[^\]]+/).join
 a = ARGV[0].scan(/(?<=from:)[\w+.-]+/).join
+# a = ARGV[0].scan(/(?<=to:)[^\]]+/).join
 b = ARGV[0].scan(/(?<=to:)[\w+.-]+/).join
+# a = ARGV[0].scan(/(?<=flags:)[^\]]+/).join
 c =  ARGV[0].scan(/(?<=flags:)[\w:+.-]+/).join
 puts a + "," + b + "," + c
