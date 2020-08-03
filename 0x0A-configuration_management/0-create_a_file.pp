86b7d08eb76ed50627ec401289fe4b7bf88c635e
yacinekedidi <kedidiyacine@gmail.com>
2020-08-03 17:20:23 +0100

    CM

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file
-file { "/tmp/holberton":
-ensure => "file",
-mode => "0744",
-owner => "www-data",
-group => "www-data",
-content => "I love Puppet",
+file { '/tmp/holberton':
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
