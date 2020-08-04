586969eabdc75fcb37a8525d57f5a33e630e304c
yacinekedidi <kedidiyacine@gmail.com>
2020-08-04 16:21:45 +0100

    puppet

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,12 @@
 # make changes to our configuration file
-file_line { 'add_line':
+file_line { 'remove password authentifcation':
     ensure => 'present',
-    line   => 'IdentityFile ~/.ssh/holberton\nPasswordAuthentication no',
     path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+}
+
+file_line { 'add line':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/holberton',
 }
