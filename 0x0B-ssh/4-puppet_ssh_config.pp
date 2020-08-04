c6d01b024c4356bc809d4b0009a80dabdc2d0bbc
yacinekedidi <kedidiyacine@gmail.com>
2020-08-04 16:12:04 +0100

    puppet

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# make changes to our configuration file
+file_line { 'add_line':
+    ensure => present,
+    line   => 'IdentityFile ~/.ssh/holberton\nPasswordAuthentication no',
+    path   => '/etc/ssh/ssh_config',
+}
