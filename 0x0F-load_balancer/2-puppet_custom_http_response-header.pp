9174f725f89915fe7f565127141ddf5e9b3629d7
yacinekedidi <kedidiyacine@gmail.com>
2020-08-14 23:08:02 +0100

    new

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,7 @@
 #  custom HTTP header response, but with Puppet.
-exec {'header add':
-command  => 'sudo apt-get update -y; 
-sudo apt-get install nginx -y; 
+
+exec { 'add Header':
+command => 'sudo apt-get -y install haproxy;
 echo "ENABLED=1" >> /etc/default/haproxy;
 printf %s "frontend load_balancer
     bind *:80
@@ -13,5 +13,7 @@ backend holb
     server 1432-web-01 35.190.159.176:80 check
     server 1432-web-02 35.229.61.48:80 check" >> /etc/haproxy/haproxy.cfg;
 service haproxy restart;',
-provider => 'shell',
+sudo service haproxy restart;
+',
+provider   => 'shell',
 }
