f230700fd96edfd1c39d4fe9f8da004940670974
yacinekedidi <kedidiyacine@gmail.com>
2020-09-14 16:09:21 +0100

    Python script

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
     print("Employee {} is done with tasks({}/{}):"
           .format(user.get("name"), len(tasks), len(todos)))
     for i in tasks:
-        print("\t{}".format(i))
+        print("\t {}".format(i))
