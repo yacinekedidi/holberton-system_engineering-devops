fa6aeaa9752689fc185378ad68754b680b22f294
yacinekedidi <kedidiyacine@gmail.com>
2020-09-17 17:29:17 +0100

    py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""function"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"user-agent": "yacinekedidi"}
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+    else:
+        r = response.json()
+        info = r.get("data")
+        children = info.get("children")
+        for post in children:
+            post_info = post.get("data")
+            title = post_info.get("title")
+            print(title)
