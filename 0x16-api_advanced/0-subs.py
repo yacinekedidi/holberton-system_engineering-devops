d9a481ec70a422f026449a7492ed6d7679cf4d11
yacinekedidi <kedidiyacine@gmail.com>
2020-09-17 16:49:15 +0100

    py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'user-agent': 'yacinekedidi'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    r = response.json()
+    info = r.get('data')
+    return info.get('subscribers')
