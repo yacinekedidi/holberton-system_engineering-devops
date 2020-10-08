24195940353b74ba1bf681cd9e7f5fa1eb146693
yacinekedidi <kedidiyacine@gmail.com>
2020-10-08 18:44:43 +0100

    new

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #  Sky is the limit, let's bring that limit higher
 exec { 'nginx':
-command => 'sed -i "5s/[0-9]\+/4096/" /etc/default/nginx; sudo service nginx restart',
+command => 'sudo echo "ULIMIT=\"-n 4096\"" >> /etc/default/nginx ; sudo service nginx restart',
 path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
